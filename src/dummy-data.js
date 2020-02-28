ad5baee156c2f45c72903cdc678977018f9fffa2
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-02-28 05:01:23 -0500

    Add addtonal data

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -41,10 +41,63 @@ const dummyData = [
             },
             {
                 username: "dennis_futbol",
-                text: "It that injustice"
-            }
+                text: "It that injustice",
+            },
         ]
-    }
-];
+    },
+        {
+            username: "lambdaschool",
+            thumbnailUrl: 'https://instagram.fric1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/82934040_174703690428797_589660801711734784_n.jpg?_nc_ht=instagram.fric1-2.fna.fbcdn.net&_nc_ohc=pdtK57Q5200AX_YUzdw&oh=2e63eea6d8f10ac5ec2a588006d811db&oe=5E898F69',
+            imageUrl: 'https://instagram.fric1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/85162549_3039515439392182_1666849314696746243_n.jpg?_nc_ht=instagram.fric1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=MRHwgd6re_kAX_fpDWW&oh=6e5a0f2b41c09d2699c5d22b9c03c533&oe=5E824CDE',
+            likes: 8259,
+            timestamp: "January 13 2020, 12:42:40 pm",
+            comments: [
+                {
+                    username: "southernhospitalityconsulting",
+                    text: "Stoked to be a #Lambdaschool Student!!"
+                },
+                {
+                    username: "mosesin.tech",
+                    text: "Truth!"
+                },   
+            
+            ]
+        },
+        {
+            username: "mosesin.tech",
+            thumbnailUrl: 'https://instagram.fric1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/72749903_443299279692573_4764835915023515648_n.jpg?_nc_ht=instagram.fric1-2.fna.fbcdn.net&_nc_ohc=P_ZSGg8L3U8AX8-UU9Y&oh=a9b4df28117eefd9c814d5834992b67f&oe=5E863A9A',
+            imageUrl: 'https://instagram.fric1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/87680986_501698207203209_7377517861827551683_n.jpg?_nc_ht=instagram.fric1-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=P3jBWn6CgEIAX_ZTmCC&oh=f15bab3938acfdcb000102a8fd8f459c&oe=5E8677C7',
+            likes: 1572,
+            timestamp: "February 19 2020, 11:27:40 am",
+            comments: [
+                {
+                    username: "southernhospitalityconsulting",
+                    text: "It might get a laugh anyway"
+                },
+                {
+                    username: "mosesin.tech",
+                    text: "I'll let you when I land my next interview."
+                },
+            ]
+        },
+        {
+            username: "southernhospitalityconsulting",
+            thumbnailUrl: 'https://instagram.fric1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/82121923_512165779683136_1289897984590348288_n.jpg?_nc_ht=instagram.fric1-2.fna.fbcdn.net&_nc_ohc=1mECUwy8EoEAX_yaIWh&oh=75e9feee727af901dc1fa010ec43cdb5&oe=5E86985E',
+            imageUrl: 'https://instagram.fric1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/81835642_166441204574801_2296869836636358742_n.jpg?_nc_ht=instagram.fric1-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=NBPEdql_fLAAX8PxiXB&oh=683e7c9f0113e6283bab6bac36258af9&oe=5E811D89',
+            likes: 800,
+            timestamp: "February 22 2020, 1:45:40 am",
+            comments: [
+                {
+                    username: "graphic.maven",
+                    text: "LOL Love the way you've been using those quotes lately!"
+                },
+                {
+                    username: "southernhospitalityconsulting",
+                    text: "Thanks, Mel! They are so much fun."
+                },
+            ]
+        }
+    
+]
 
 export default dummyData;
